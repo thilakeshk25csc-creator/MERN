89f56847f62bccffc4527634d802c866e259c18b
thilakeshk25csc-creator <thilakeshk25csc@vetias.ac.in>
2026-06-12 14:45:00 +0530

    added data type

---
+++ datatype.js
@@ -0,0 +1,23 @@
+//Number
+console.log("number");
+var age=30
+console.log(age)
+
+//string
+
+console.log("string");
+var name="hii"
+console.log(name);
+
+//boolean
+console.log("boolean");
+var isstudent=true;
+console.log(isstudent);
+
+//null
+console.log("NULL");
+var empty=null;
+console.log(empty)
+
+
+
