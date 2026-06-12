f1c3d24742d2d24c667ecc3fe4e34353a6ce7f8d
thilakeshk25csc-creator <thilakeshk25csc@vetias.ac.in>
2026-06-12 11:29:16 +0530

    added operators

--- operators.js
+++ operators.js
@@ -1,14 +1,25 @@
-/*var a=20
+//Arithmetic operators
+
+/*
+
+var a=20
 var b=53
 console.log(a+b)
 console.log(a-b)
 console.log(a*b)
-console.log(a/b)*/
+console.log(a/b)
+
+*/
 
 //assigment operators
+/*
+
 var c=34
 
 console.log(c+=10)
 console.log(c-=10)
 console.log(c*=10)
 console.log(c/=10)
+
+*/
+
