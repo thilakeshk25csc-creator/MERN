fd602350cef0d457097c868f7e46905217b09038
thilakeshk25csc-creator <thilakeshk25csc@vetias.ac.in>
2026-06-15 10:35:42 +0530

    Added condition.js

---
+++ Condition.js
@@ -0,0 +1,15 @@
+var signal="White"
+
+switch(signal){
+    case "Red":
+        console.log("Stop");
+        break;
+    case "Yellow":
+        console.log("Wait!");
+        break;
+    case "Green":
+        console.log("Go");
+        break;
+    default:
+        console.log("Invalid signal");
+}
